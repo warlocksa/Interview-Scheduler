ef57c3731219ebc111d8c63ee26884b9c618f03d
warlocksa <warlock_sa@hotmail.com>
2022-04-21 22:16:42 -0600

    add the selector and selector test and pass the test

---
+++ src/helpers/selectors.js
@@ -0,0 +1,21 @@
+export function getAppointmentsForDay(state, day) {
+  //... returns an array of appointments for that day
+  let result = [];
+  const filteredday = state.days.filter(Day => Day.name === day)
+ 
+  if (filteredday.length === 0) {
+    return result;
+  }
+  if (filteredday.length > 0) {
+    for (let appointmentId of filteredday[0].appointments) {
+      for (let Appointment of Object.keys(state.appointments)) {
+        if (appointmentId == Appointment) {
+          result.push(state.appointments[Appointment]);
+        }
+      }
+    }
+    return result;
+  }
+
+}
+
